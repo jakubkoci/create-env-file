6e82b5caecff4d846c4c7e500d635522b4a4ec29
Jakub Koci <jakub.koci@gmail.com>
2021-04-01 18:51:19 +0200

    Print file stats

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   fs.writeFileSync(".env", "some text", { encoding: "utf-8" });
+  console.log(fs.statSync('.env'))
 } catch (error) {
   core.setFailed(error.message);
 }
