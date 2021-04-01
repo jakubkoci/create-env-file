61b94b1f4a8595964af06c2b095157e6a427d5cd
Jakub Koci <jakub.koci@gmail.com>
2021-04-01 18:26:36 +0200

    Add write to file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  fs.writeFileSync(".env", "some text", { encoding: "utf-8" });
 } catch (error) {
   core.setFailed(error.message);
 }
